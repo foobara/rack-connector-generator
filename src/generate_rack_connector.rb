5ec72503474d78c111712b40710f88103c5c2259
Samuel O'Daniels <35481602+Samuelodan@users.noreply.github.com>
2026-09-22 06:37:53 +0100

    Bump deps, enable deprecation warnings, and fix related issues

--- src/generate_rack_connector.rb
+++ src/generate_rack_connector.rb
@@ -1,5 +1,3 @@
-require "pathname"
-
 require_relative "rack_connector_config"
 
 module Foobara
@@ -33,9 +31,9 @@ module Foobara
         # TODO: delegate this to base_generator
         def templates_dir
           # TODO: implement this?
-          # :nocov:
+          # simplecov:disable
           "#{__dir__}/../templates"
-          # :nocov:
+          # simplecov:enable
         end
 
         def add_initial_elements_to_generate
--- src/generators/gemfile_generator.rb
+++ src/generators/gemfile_generator.rb
@@ -30,9 +30,9 @@ module Foobara
               "#{match.pre_match}\n#{new_entry}\n#{match}#{match.post_match}"
             else
               # TODO: maybe print a warning and return the original Gemfile
-              # :nocov:
+              # simplecov:disable
               raise "Not sure how to inject rack_connector into the Gemfile"
-              # :nocov:
+              # simplecov:enable
             end
           end
         end
